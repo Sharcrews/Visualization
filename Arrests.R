8b9b748672b563ec32a16f3e95675f8cc7230364
Sharcrews <70011495+Sharcrews@users.noreply.github.com>
2022-03-15 06:15:05 -0400

    Commmit2

--- Arrests.R
+++ Arrests.R
@@ -1,1 +1,21 @@
-titanic
+library(tidyverse)
+
+
+#Load Toronto Marijuana Arrest Data
+Arrests <- carData::Arrests
+str(Arrests)
+describe(Arrests)
+
+summary(Arrests)
+plot(Arrests)
+
+#Histogram
+ggplot(Arrests, aes(age, fill = colour)) + geom_histogram()
+
+
+#bar plot
+ggplot(Arrests, aes(year, fill = sex)) + geom_bar() 
+
+ggplot(Arrests, aes(checks, age , color = factor(checks))) + 
+  geom_boxplot()
+
